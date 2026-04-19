09e8b0db6129d447477a609bc03756a2a8e63c62
naninani2xx1 <nhchuong2001@gmail.com>
2026-04-19 23:02:12 +0700

    [update]: RoutingLoader

--- src/Routing/AmzsSettingLoader.php
+++ src/Routing/AmzsSettingLoader.php
@@ -2,12 +2,21 @@
 
 namespace AmzsCMS\SettingBundle\Routing;
 
+use AmzsCMS\CoreBundle\Constant\CoreConfig;
 use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\RouteCollection;
 
 class AmzsSettingLoader extends Loader
 {
     private $isLoaded = false;
+    private ParameterBagInterface $parameterBag;
+    public function __construct(?string $env = null, ?ParameterBagInterface $parameterBag = null)
+    {
+        parent::__construct($env);
+        $this->parameterBag = $parameterBag;
+    }
+
     public function load($resource, ?string $type = null): RouteCollection
     {
         if (true === $this->isLoaded) {
@@ -19,7 +28,7 @@ class AmzsSettingLoader extends Loader
         /** @var $importedRoutes */
         $importedRoutes = $this->import($resource, $type);
         $routes->addCollection($importedRoutes);
-        $routes->addPrefix('/cms');
+        $routes->addPrefix($this->parameterBag->get(CoreConfig::PATH_TREE_BUILDER.'.routing.prefix_app'));
 
         $this->isLoaded = true;
 
